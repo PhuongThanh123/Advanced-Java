148c873b83ce929f49fca35da4cef674831141a9
PhuongThanh123 <lephuongthanh1502@gmail.com>
2016-08-22 21:42:04 +0700

    new

---
+++ Chapter1LePhuongThanhPart3/src/exercise110/MainClass110ManageInformation.java
@@ -0,0 +1,24 @@
+package exercise110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainClass110ManageInformation {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("===========MANAGE INFORMATION===========");
+		System.out.println("Enter Name: ");
+		String name=input.readLine();
+		
+		System.out.println("Enter Birthday: ");
+		String birthday=input.readLine();
+		
+		System.out.println("Enter Address: ");
+		
+
+	}
+
+}
