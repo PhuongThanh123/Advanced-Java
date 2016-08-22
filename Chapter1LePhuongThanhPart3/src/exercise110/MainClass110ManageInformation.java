16a69f2bc9a2c3725f5a8a166107f657e5e862a6
PhuongThanh123 <lephuongthanh1502@gmail.com>
2016-08-22 21:50:29 +0700

    new

--- Chapter1LePhuongThanhPart3/src/exercise110/MainClass110ManageInformation.java
+++ Chapter1LePhuongThanhPart3/src/exercise110/MainClass110ManageInformation.java
@@ -8,6 +8,8 @@ public class MainClass110ManageInformation {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		Teacher teacher=new Teacher();
+		Student student=new Student();
 		
 		System.out.println("===========MANAGE INFORMATION===========");
 		System.out.println("Enter Name: ");
@@ -17,8 +19,48 @@ public class MainClass110ManageInformation {
 		String birthday=input.readLine();
 		
 		System.out.println("Enter Address: ");
+		String address=input.readLine();
 		
+		System.out.println("Enter Phone: ");
+		String phone=input.readLine();
+		
+		
+		
+		boolean flag=true;
+		while(flag){
+			
+		}
 
 	}
+	public static void selectPerson() throws NumberFormatException, IOException{
+		BufferedReader input=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Seclect Teacher or Student: \n1: Teacher \n2: Student");
+		int number=Integer.parseInt(input.readLine());
+		switch(number){
+		case 1:
+			System.out.println("-------ADD infor of Teacher--------");
+			System.out.println("Enter Class Leader: ");
+			String classLeader=input.readLine();
+			
+			System.out.println("Enter Coefficients Salary: ");
+			double coefficientsSalary=Double.parseDouble(input.readLine());
+			
+			System.out.println("Enter Allowance: ");
+			double allowance=Integer.parseInt(input.readLine());
+			//add code
+			break;
+		case 2:
+			System.out.println("-------ADD infor of Student--------");
+			System.out.println("Enter Name Class: ");
+			String nameClass=input.readLine();
+			
+			System.out.println("Entern MarkI: ");
+			double markI=Integer.parseInt(input.readLine());
+			
+			System.out.println("Entern MarkII: ");
+			double markII=Integer.parseInt(input.readLine());
+		}
+	}
+	
 
 }
--- Chapter1LePhuongThanhPart3/src/exercise110/Teacher.java
+++ Chapter1LePhuongThanhPart3/src/exercise110/Teacher.java
@@ -1,5 +1,42 @@
 package exercise110;
 
-public class Teacher {
-
+public class Teacher extends Human {
+	private String classLeader;
+	private double coefficientsSalary;
+	private double allowance;
+	public String getClassLeader() {
+		return classLeader;
+	}
+	public void setClassLeader(String classLeader) {
+		this.classLeader = classLeader;
+	}
+	public double getCoefficientsSalary() {
+		return coefficientsSalary;
+	}
+	public void setCoefficientsSalary(double coefficientsSalary) {
+		this.coefficientsSalary = coefficientsSalary;
+	}
+	public double getAllowance() {
+		return allowance;
+	}
+	public Teacher() {
+		super();
+	}
+	public void setAllowance(double allowance) {
+		this.allowance = allowance;
+	}
+	public Teacher(String name, String birthday, String address, String phone,
+			String classLeader, double coefficientsSalary, double allowance) {
+		super();
+		this.classLeader = classLeader;
+		this.coefficientsSalary = coefficientsSalary;
+		this.allowance = allowance;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString()+ "Class Leader: "+classLeader+"Coefficients Salary: "+coefficientsSalary+"Allowance: "+allowance;
+	}
+	
 }
